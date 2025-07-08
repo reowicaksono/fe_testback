610f131dcb0585e08aa13f9779be742eac7598cb
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-07-08 15:50:35 +0700

    add: helpers translate error

---
+++ src/utils/error_translate.ts
@@ -0,0 +1,32 @@
+
+
+export type ErrorMap = {
+  [field: string]: string;
+};
+
+
+export const translateErrorMessage = (error: any): ErrorMap =>{
+  const errors: ErrorMap = {};
+
+  if (error?.response?.data) {
+    const data = error.response.data;
+
+    // Jika data langsung objek field-error
+    if (typeof data === 'object' && !Array.isArray(data)) {
+      Object.keys(data).forEach((key) => {
+        errors[key] = data[key];
+      });
+    }
+
+    // Jika ada pesan umum
+    if (data.message && typeof data.message === 'string') {
+      errors.general = data.message;
+    }
+  } else if (error?.message) {
+    errors.general = error.message;
+  } else {
+    errors.general = 'Terjadi kesalahan tidak diketahui.';
+  }
+
+  return errors;
+}
